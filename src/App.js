ab6eea0f8d9291e8d2446dbda21efd805a17c777
Oussema1232 <oussema.ben.soltana1@gmail.com>
2020-01-03 21:47:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, {useState,useReducer } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state,action){
+  switch (action.type){
+    case "add-todo":
+      return{
+        todos:[{text:action.value,delete:false,complete:false},...state.todos]
+      };
+      case "toggle":
+        return{
+          todos: state.todos.map((todo,index)=>
+            index===action.index?{
+              ...todo,
+              complete:!todo.complete
+            }:
+            todo
+          )   
+        };
+        case "delete":
+          return{
+            todos: state.todos.map((todo,index)=>
+            (
+              index===action.index?{
+                ...todo,
+                delete:!todo.delete
+              }:
+              todo
+            ))
+          }
+        default:
+          return state;
+          
+  }
 }
+
+function App(){
+  const [{todos},dispatch]=useReducer(reducer,{todos:[]});
+  const [value,setValue]=useState("");
+  
+return(
+  <div className="App">
+    <form onSubmit={
+      (e)=>{
+        e.preventDefault();
+        setValue("");
+      }
+    }>
+    <input value={value} onChange={
+    (e)=>setValue(e.target.value)}/>
+    <button value={value} onClick={()=>dispatch({type:"add-todo",value})}>Add</button>
+    </form>
+    
+      {todos.map((todo,index)=>(
+        <div>
+        <div key={todo.text}
+        onClick={()=>dispatch({type:"toggle",index})}
+        style={{
+          textDecoration:todo.complete? "line-through":"",
+          userSelect:"none",
+          display:todo.delete?"none":"inline-block",
+          width:100 
+        }}>
+          {todo.text}</div>
+          {!(todo.delete)?<button value={todo.text} onClick={()=>dispatch({type:"delete",index})}>X</button>:""}
+          </div>
+      ))}
+    
+  </div>
+);}
 
 export default App;
